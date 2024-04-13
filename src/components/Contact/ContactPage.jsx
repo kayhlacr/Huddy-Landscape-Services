6ea5679e1c31f249808b8023bb4729fba4517834
kayhlacr <kayhla@student.dev-island.org>
2024-04-12 23:41:43 -1000

    made contact page mobile compatabile and added option for user to click icon as well

--- src/components/Contact/ContactPage.jsx
+++ src/components/Contact/ContactPage.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./ContactPage.css";
 import phoneIcon from "../../assets/phoneIcon.png";
 import instagramIcon from "../../assets/instagramIcon.png";
@@ -12,36 +11,60 @@ const ContactPage = () => {
       <div className="contact-info">
         <div className="contact-column">
           <div className="contact-item">
-            <img src={phoneIcon} alt="Phone" />
-            <a href="tel:808-515-3332">808-515-3332</a>
+            <div className="contact-content">
+              <a href="tel:808-515-3332">
+                <img src={phoneIcon} alt="Phone" />
+              </a>
+              <a href="tel:808-515-3332">808-515-3332</a>
+            </div>
           </div>
           <div className="contact-item">
-            <img src={emailIcon} alt="Email" />
-            <a href="mailto:huddylandscapeservice@gmail.com">
-              huddylandscapeservice@gmail.com
-            </a>
+            <div className="contact-content">
+              <a href="mailto:huddylandscapeservice@gmail.com">
+                <img src={emailIcon} alt="Email" />
+              </a>
+              <a href="mailto:huddylandscapeservice@gmail.com">
+                huddylandscapeservice@gmail.com
+              </a>
+            </div>
           </div>
         </div>
         <div className="contact-column">
           <div className="contact-item">
-            <img src={formIcon} alt="Form" />
-            <a
-              href="https://docs.google.com/forms/d/e/1FAIpQLSf0lEBJ2hOcfeMMqUw2wWNW9P2xCbYaePyrBLiQ6_X5jSFXYQ/viewform"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Need landscaping work done? Fill out our form
-            </a>
+            <div className="contact-content">
+              <a
+                href="https://docs.google.com/forms/d/e/1FAIpQLSf0lEBJ2hOcfeMMqUw2wWNW9P2xCbYaePyrBLiQ6_X5jSFXYQ/viewform"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={formIcon} alt="Form" />
+              </a>
+              <a
+                href="https://docs.google.com/forms/d/e/1FAIpQLSf0lEBJ2hOcfeMMqUw2wWNW9P2xCbYaePyrBLiQ6_X5jSFXYQ/viewform"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Need landscaping work done? Fill out our form
+              </a>
+            </div>
           </div>
           <div className="contact-item">
-            <img src={instagramIcon} alt="Instagram" />
-            <a
-              href="https://www.instagram.com/huddylandscapeserviceinc/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Follow Us on Instagram
-            </a>
+            <div className="contact-content">
+              <a
+                href="https://www.instagram.com/huddylandscapeserviceinc/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src={instagramIcon} alt="Instagram" />
+              </a>
+              <a
+                href="https://www.instagram.com/huddylandscapeserviceinc/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Follow Us on Instagram
+              </a>
+            </div>
           </div>
         </div>
       </div>
