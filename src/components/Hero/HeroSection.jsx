6a7952d128d5c53b70fba6d04efb426217d78eeb
Kayhlacr <kayhla@student.dev-island.org>
2024-05-30 09:53:06 -1000

    added an apos to h1 on home page

--- src/components/Hero/HeroSection.jsx
+++ src/components/Hero/HeroSection.jsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
       <img src={elephantEar} alt="Plant" />
       <div className="hero-content">
         <h1>Creating Vibrant Landscapes</h1>
-        <h1>Enriching Kauai Community</h1>
+        <h1>Enriching Kaua&apos;i Community</h1>
         <a
           href="https://docs.google.com/forms/d/e/1FAIpQLSf0lEBJ2hOcfeMMqUw2wWNW9P2xCbYaePyrBLiQ6_X5jSFXYQ/viewform"
           target="_blank"
