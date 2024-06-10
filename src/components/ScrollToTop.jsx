c32073ddeac5021b56914b5f061af44371c070db
Kayhlacr <kayhla@student.dev-island.org>
2024-06-09 17:11:53 -1000

    adjusted scroll to op to account for the 60px nav bar so content starts at the bottom of the nav bar

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,14 +1,12 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-const ScrollToTop = ({ yOffset = 0, setyOffset, children }) => {
-  const copyYOffset = yOffset;
+const ScrollToTop = ({ children }) => {
   const location = useLocation();
 
   useEffect(() => {
-    window.scrollTo({ top: copyYOffset, left: 0, behavior: "instant" });
-    setyOffset(0);
-  }, [location, copyYOffset, setyOffset]);
+    window.scrollTo({ top: -60, left: 0, behavior: "instant" });
+  }, [location]);
 
   return <>{children}</>;
 };
